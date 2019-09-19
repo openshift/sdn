6bf41feed827ab02199d96e37691fe200aa51504
Casey Callendrello <cdc@redhat.com>
2019-09-19 15:54:11 +0200

    DNM: more informer logging

--- pkg/openshift-sdn/informers.go
+++ pkg/openshift-sdn/informers.go
@@ -1,13 +1,16 @@
 package openshift_sdn
 
 import (
+	"fmt"
 	"net"
 	"net/http"
 	"time"
 
+	v1 "k8s.io/api/core/v1"
 	kinformers "k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/klog"
 
 	networkclient "github.com/openshift/client-go/network/clientset/versioned"
 	networkinformers "github.com/openshift/client-go/network/informers/externalversions"
@@ -43,6 +46,9 @@ func (sdn *OpenShiftSDN) buildInformers() error {
 
 	kubeInformers := kinformers.NewSharedInformerFactory(kubeClient, sdn.ProxyConfig.IPTables.SyncPeriod.Duration)
 	networkInformers := networkinformers.NewSharedInformerFactory(networkClient, defaultInformerResyncPeriod)
+
+	kubeInformers.Core().V1().Services().Informer().AddEventHandler(eventHandlingLogger{})
+	kubeInformers.Core().V1().Endpoints().Informer().AddEventHandler(eventHandlingLogger{})
 
 	sdn.informers = &informers{
 		KubeClient:    kubeClient,
@@ -96,4 +102,32 @@ func defaultClientTransport(rt http.RoundTripper) http.RoundTripper {
 func applyClientConnectionOverrides(kubeConfig *rest.Config) {
 	kubeConfig.QPS = 10.0
 	kubeConfig.Burst = 20
+}
+
+type eventHandlingLogger struct{}
+
+func describe(obj interface{}) string {
+	if svc, ok := obj.(*v1.Service); ok {
+		if svc.Spec.Type == v1.ServiceTypeExternalName {
+			return fmt.Sprintf("externalname service %s/%s", svc.Namespace, svc.Name)
+		} else if svc.Spec.ClusterIP == v1.ClusterIPNone {
+			return fmt.Sprintf("headless service %s/%s", svc.Namespace, svc.Name)
+		} else {
+			return fmt.Sprintf("service %s/%s", svc.Namespace, svc.Name)
+		}
+	} else if ep, ok := obj.(*v1.Endpoints); ok {
+		return fmt.Sprintf("endpoints %s/%s (%d)", ep.Namespace, ep.Name, len(ep.Subsets))
+	} else {
+		return fmt.Sprintf("%T", obj)
+	}
+}
+
+func (eventHandlingLogger) OnAdd(obj interface{}) {
+	klog.Infof("ADD %s", describe(obj))
+}
+func (eventHandlingLogger) OnUpdate(oldObj, newObj interface{}) {
+	klog.Infof("UPDATE %s -> %s", describe(oldObj), describe(newObj))
+}
+func (eventHandlingLogger) OnDelete(obj interface{}) {
+	klog.Infof("DELETE %s", describe(obj))
 }
