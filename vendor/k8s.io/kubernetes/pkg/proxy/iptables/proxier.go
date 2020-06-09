28e351fa01b487ce9629cb3e968dca7368c94b9b
Jacob Tanenbaum <jtanenba@redhat.com>
2020-06-09 14:55:52 -0400

    grumble

--- vendor/k8s.io/kubernetes/pkg/proxy/iptables/proxier.go
+++ vendor/k8s.io/kubernetes/pkg/proxy/iptables/proxier.go
@@ -1540,9 +1540,12 @@ func (proxier *Proxier) syncProxyRules() {
 	proxier.iptablesData.Write(proxier.natChains.Bytes())
 	proxier.iptablesData.Write(proxier.natRules.Bytes())
 
-	klog.V(2).Infof("Restoring iptables rules: %s", proxier.iptablesData.Bytes())
+	//klog.V(2).Infof("Restoring iptables rules: %s", proxier.iptablesData.Bytes())
+	iptables_dump := proxier.iptablesData.Bytes()
 	err = proxier.iptables.RestoreAll(proxier.iptablesData.Bytes(), utiliptables.NoFlushTables, utiliptables.RestoreCounters)
 	if err != nil {
+		klog.Errorf("___---^^^ iptablesData.Bytes() ^^^---___\n%s\n ___---^^^ end of iptablesData.Bytes() ^^^---___", iptables_dump)
+
 		klog.Errorf("Failed to execute iptables-restore: %v", err)
 		metrics.IptablesRestoreFailuresTotal.Inc()
 		// Revert new local ports.
